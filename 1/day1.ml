b1801bbbda2b826edf2de22fb7c59e4f53c57bf1
Tevqoon <jure.smolar@gmail.com>
2020-12-01 16:34:46 +0100

    Day 1

---
+++ 1/day1.ml
@@ -0,0 +1,62 @@
+let preberi_datoteko ime_datoteke =
+  let chan = open_in ime_datoteke in
+  let vsebina = really_input_string chan (in_channel_length chan) in
+  close_in chan;
+  vsebina
+
+let izpisi_datoteko ime_datoteke vsebina =
+  let chan = open_out ime_datoteke in
+  output_string chan vsebina;
+  close_out chan
+
+let string_to_list string = 
+  string
+  |> String.split_on_char '\n'
+  |> List.filter (fun s -> s <> "");;
+
+let rec print_list = function
+  | [] -> ()
+  | head :: tail -> print_int head ; print_string " " ; print_list tail
+
+let naloga1 string = 
+  let nums = List.map int_of_string (string_to_list string) in
+  let rec r l1 l2 = match l1, l2 with
+    | [], _ -> "None"
+    | x::xs, y::ys when x + y = 2020 -> string_of_int (x * y)
+    | frst, y::ys -> r frst ys
+    | x::xs, [] -> r xs xs
+  in r nums nums
+
+let naloga2 string = 
+  let nums = List.map int_of_string (string_to_list string) in
+  let rec r l1 l2 l3 = match l1, l2, l3 with
+    | [], _, _ -> "None"
+    | x::xs, y::ys, z::zs when x + y + z = 2020 -> string_of_int (x * y * z)
+    | x, y, z::zs -> r x y zs
+    | x, y::ys, [] -> r x ys ys
+    | x::xs, [], _ ->  r xs xs xs
+  in r nums nums nums
+
+let main () =
+  let day = "1" in
+  print_endline ("Solving DAY: " ^ day);
+  let input_data = preberi_datoteko (day ^ "/day_" ^ day ^ ".in") in
+  let p1_start = Sys.time () in
+  let part1 = naloga1 input_data in
+  let t1_time = Sys.time () -. p1_start in
+  print_endline "PART 1:";
+  print_endline part1;
+  print_endline ("Taken: " ^ string_of_float t1_time ^ "s");
+  izpisi_datoteko (day ^ "/day_" ^ day ^ "_1.out") part1;
+  
+  let p2_start = Sys.time () in
+  let part2 = naloga2 input_data in
+  let t2_time = Sys.time () -. p2_start in
+  print_endline "PART 2:";
+  print_endline part2;
+  print_endline ("Taken: " ^ string_of_float t2_time ^ "s");
+  print_endline ("Total: " ^ string_of_float (t1_time +. t2_time) ^ "s");
+  izpisi_datoteko (day ^ "/day_" ^ day ^ "_2.out") part2;
+  ()
+
+let _ = main ()
---
+++ template.ml
@@ -0,0 +1,44 @@
+let preberi_datoteko ime_datoteke =
+  let chan = open_in ime_datoteke in
+  let vsebina = really_input_string chan (in_channel_length chan) in
+  close_in chan;
+  vsebina
+
+let izpisi_datoteko ime_datoteke vsebina =
+  let chan = open_out ime_datoteke in
+  output_string chan vsebina;
+  close_out chan
+
+let string_to_list string = 
+  string
+  |> String.split_on_char '\n'
+  |> List.filter (fun s -> s <> "");;
+
+let naloga1 string = ""
+
+let naloga2 string = ""
+
+let main () =
+  let day = "INSERT_DAY_HERE" in
+  print_endline ("Solving DAY: " ^ day);
+  let input_data = preberi_datoteko (day ^ "/day_" ^ day ^ ".in") in
+
+  let p1_start = Sys.time () in
+  let part1 = naloga1 input_data in
+  let t1_time = Sys.time () -. p1_start in
+  print_endline "PART 1:";
+  print_endline part1;
+  print_endline ("Taken: " ^ string_of_float t1_time ^ "s");
+  izpisi_datoteko (day ^ "/day_" ^ day ^ "_1.out") part1;
+  
+  let p2_start = Sys.time () in
+  let part2 = naloga2 input_data in
+  let t2_time = Sys.time () -. p2_start in
+  print_endline "PART 2:";
+  print_endline part2;
+  print_endline ("Taken: " ^ string_of_float t2_time ^ "s");
+  print_endline ("Total: " ^ string_of_float (t1_time +. t2_time) ^ "s");
+  izpisi_datoteko (day ^ "/day_" ^ day ^ "_2.out") part2;
+  ()
+
+let _ = main ()
